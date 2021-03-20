ccb3cbccd34e82724d1c7bd2fe2291fd9c2b9584
Skyrich_u <petercha2000@gmail.com>
2021-03-20 10:53:44 +0000

    box to b

--- ft_printf/srcs/boxing.c
+++ ft_printf/srcs/boxing.c
@@ -12,87 +12,87 @@
 
 #include "ft_printf.h"
 
-static void	boxing_char(t_flag *flag, t_box *box)
+static void	boxing_char(t_flag *f, t_box *b)
 {
-	box->value_len = 1;
-	if (flag->type & PERCENT)
-		box->value = '%';
-	if (flag->flag & ZERO && !(flag->flag & LEFT))
-		box->zero = flag->width - box->value_len;
+	b->value_len = 1;
+	if (f->type & PERCENT)
+		b->value = '%';
+	if (f->flag & ZERO && !(f->flag & LEFT))
+		b->zero = f->width - b->value_len;
 }
 
-static void	boxing_string(t_flag *flag, t_box *box)
+static void	boxing_string(t_flag *f, t_box *b)
 {
-	box->value_len = 6;
-	if (box->value)
-		box->value_len = ft_strlen((char *)box->value);
-	if (flag->precision != -1 && flag->precision < box->value_len)
-		box->value_len = flag->precision;
-	if (flag->flag & ZERO && !(flag->flag & LEFT))
-		box->zero = flag->width - box->value_len;
+	b->value_len = 6;
+	if (b->value)
+		b->value_len = ft_strlen((char *)b->value);
+	if (f->precision != -1 && f->precision < b->value_len)
+		b->value_len = f->precision;
+	if (f->flag & ZERO && !(f->flag & LEFT))
+		b->zero = f->width - b->value_len;
 }
 
-static void	boxing_hexa(t_flag *flag, t_box *box)
+static void	boxing_hexa(t_flag *f, t_box *b)
 {
 	char	*base;
 
-	if (flag->type & POINTER || flag->flag & HASH)
-		box->prefix = 'x' - 32 * ((flag->type & CHEXA) > 0);
+	if (f->type & POINTER || f->flag & HASH)
+		b->prefix = 'x' - 32 * ((f->type & CHEXA) > 0);
 	base = "0123456789abcdef";
-	if (flag->type & CHEXA)
+	if (f->type & CHEXA)
 		base = "0123456789ABCDEF";
-	ft_memcpy(box->base, base, (box->base_len = 16));
-	box->value_len = ft_get_number_len((t_ull)box->value, 16);
-	if (box->value == 0 && !(flag->type & POINTER))
-		box->prefix = 0;
-	if (flag->precision == 0 && box->value == 0)
-		box->value_len = 0;
-	if (flag->precision != -1)
-		box->zero = flag->precision - box->value_len;
-	else if (flag->flag & ZERO && !(flag->flag & LEFT))
+	ft_memcpy(b->base, base, (b->base_len = 16));
+	b->value_len = ft_get_number_len((t_ull)b->value, 16);
+	if (b->value == 0 && !(f->type & POINTER))
+		b->prefix = 0;
+	if (f->precision == 0 && b->value == 0)
+		b->value_len = 0;
+	if (f->precision != -1)
+		b->zero = f->precision - b->value_len;
+	else if (f->flag & ZERO && !(f->flag & LEFT))
 		b->zero = f->width - b->value_len - (b->sign > 0) - (b->prefix > 0) * 2;
 }
 
-static void	boxing_number(t_flag *flag, t_box *box)
+static void	boxing_number(t_flag *f, t_box *b)
 {
-	if ((flag->flag & SPACE))
-		box->sign = ' ';
-	if ((flag->flag & PLUS))
-		box->sign = '+';
-	if (box->value < 0)
+	if ((f->flag & SPACE))
+		b->sign = ' ';
+	if ((f->flag & PLUS))
+		b->sign = '+';
+	if (b->value < 0)
 	{
-		box->sign = '-';
-		box->value = -box->value;
+		b->sign = '-';
+		b->value = -b->value;
 	}
-	ft_memcpy(box->base, "0123456789", (box->base_len = 10));
-	box->value_len = ft_get_number_len((t_ull)box->value, 10);
-	if (flag->precision == 0 && box->value == 0)
-		box->value_len = 0;
-	if (flag->precision != -1)
-		box->zero = flag->precision - box->value_len;
-	else if (flag->flag & ZERO && !(flag->flag & LEFT))
-		box->zero = flag->width - box->value_len - (box->sign > 0) - (box->prefix > 0) * 2;
+	ft_memcpy(b->base, "0123456789", (b->base_len = 10));
+	b->value_len = ft_get_number_len((t_ull)b->value, 10);
+	if (f->precision == 0 && b->value == 0)
+		b->value_len = 0;
+	if (f->precision != -1)
+		b->zero = f->precision - b->value_len;
+	else if (f->flag & ZERO && !(f->flag & LEFT))
+		b->zero = f->width - b->value_len - (b->sign > 0) - (b->prefix > 0) * 2;
 }
 
 int			print_arg(t_flag *flag, t_8byte arg)
 {
 	int		len;
-	t_box	box;
+	t_box	b;
 
-	ft_bzero(&box, sizeof(t_box));
-	box.value = arg;
+	ft_bzero(&b, sizeof(t_box));
+	b.value = arg;
 	if (flag->type & NUMBER)
 		return (0);
 	else if (flag->type & (PERCENT | CHAR))
-		boxing_char(flag, &box);
+		boxing_char(flag, &b);
 	else if (flag->type & STRING)
-		boxing_string(flag, &box);
+		boxing_string(flag, &b);
 	else if (flag->type & (DIGIT | UDIGIT))
-		boxing_number(flag, &box);
+		boxing_number(flag, &b);
 	else if (flag->type & (HEXA | CHEXA | POINTER))
-		boxing_hexa(flag, &box);
-	len = box.value_len + ft_max(box.zero, 0) + (box.sign > 0) + (box.prefix > 0) * 2;
-	box.margin = ft_max(flag->width - len, 0);
-	put_all(flag, &box);
-	return (box.margin + len);
+		boxing_hexa(flag, &b);
+	len = b.value_len + ft_max(b.zero, 0) + (b.sign > 0) + (b.prefix > 0) * 2;
+	b.margin = ft_max(flag->width - len, 0);
+	put_all(flag, &b);
+	return (b.margin + len);
 }
