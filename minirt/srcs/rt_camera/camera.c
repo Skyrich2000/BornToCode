b8454467917687626215456f0b9f41badd6e5461
su <wkdlfflxh@naver.com>
2021-04-11 01:34:53 +0900

    norm

--- minirt/srcs/rt_camera/camera.c
+++ minirt/srcs/rt_camera/camera.c
@@ -1,14 +1,13 @@
 #include "minirt.h"
 
-
 void	set_camera(t_camera *cam, t_vec pos, double height, double ratio)
 {
 	cam->pos = pos;
 	cam->view_width = ratio * 2;
 	cam->view_height = 2;
-	cam->focal_length = vec(0, 0, 1); // 고정값
+	cam->focal_length = vec(0, 0, 1);
 	cam->horizon = vec(cam->view_width, 0, 0);
 	cam->vertical = vec(0, cam->view_height, 0);
 	cam->low_left_corner = vec_cal((t_vec[4]){cam->pos, cam->horizon, cam->vertical, cam->focal_length}, \
-									(double[4]){1, -0.5, -0.5, -1}, 4);
+									(double [4]){1, -0.5, -0.5, -1}, 4);
 }
--- minirt/srcs/rt_library/library.c
+++ minirt/srcs/rt_library/library.c
@@ -5,17 +5,17 @@ double	rand_num(int anti, int min, int max)
 	if (anti == 0)
 		return (0);
 	if (min < max)
-		return ((double)(min + (max - min)) * rand_num(1, 0, 0));
+		return ((double)(min + (max - min)) *rand_num(1, 0, 0));
 	else
 		return ((double)rand() / (double)RAND_MAX);
 }
 
-int trgb(int t, int r, int g, int b)
+int	trgb(int t, int r, int g, int b)
 {
 	return (t << 24 | r << 16 | g << 8 | b);
 }
 
-double clamp(double n, double min, double max)
+double	clamp(double n, double min, double max)
 {
 	if (n < min)
 		return (min);
