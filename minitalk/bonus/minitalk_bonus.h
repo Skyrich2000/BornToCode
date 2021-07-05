ecc385eb353e12f6b58a76b9b0316665bb777060
ycha <petercha2000@gmail.com>
2021-07-05 21:56:48 +0900

    minitalk done

---
+++ minitalk/bonus/minitalk_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ycha <ycha@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/04 02:14:13 by ycha              #+#    #+#             */
+/*   Updated: 2021/07/05 15:56:46 by ycha             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+
+# include <unistd.h>
+# include <signal.h>
+
+# define OK 1
+# define ERROR 0
+
+void		ft_putnbr_fd(int n, int fd);
+void		ft_putstr_fd(char *s, int fd);
+void		ft_putchar_fd(char c, int fd);
+int			ft_atoi(char *str, int *data);
+int			is_num(char *str);
+
+#endif
