11025e38ab699fb2b75034e9dc34b14c9f4d5de3
Skyrich2000M <petercha2000@gmail.com>
2021-07-15 04:12:52 +0900

    so long player image speed fixed

--- so_long/main.c
+++ so_long/main.c
@@ -104,7 +104,7 @@ int		loop(void *param)
 		
 	}
 
-	reset_canvas();
+	draw_background();
 	node = g()->instances->next;
 	while (node)
 	{
@@ -154,7 +154,7 @@ t_instance	*create_player_instance(int x, int y)
 	new->obj.player.hp = 0;
 	new->spr = g()->spr_player_idle;
 	new->img_node = new->spr->imgs->next;
-	new->img_speed = 5;
+	new->img_speed = 10;
 	new->x = x;
 	new->y = y;
 	new->step = obj_player_step;
@@ -176,12 +176,14 @@ void		obj_player_step(t_instance *this)
 		{
 			this->spr = g()->spr_player_move_right;
 			this->img_node = this->spr->imgs->next;
+			this->img_speed = 5;
 			mv = 1; // go right
 		}
 		else if (mv == 1) // if going right
 		{
 			this->spr = g()->spr_player_idle;
 			this->img_node = this->spr->imgs->next;
+			this->img_speed = 10;
 			mv = 0; // stop
 		}
 		time = 0;
