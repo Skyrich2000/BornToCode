dc5d366c578b31e008b2e73aa8889d01149f8c13
Skyrich2000M <petercha2000@gmail.com>
2021-07-24 22:58:19 +0900

    so long dont change pointer

--- so_long/src/asset/script/player/scr_player_get_spr.c
+++ so_long/src/asset/script/player/scr_player_get_spr.c
@@ -7,7 +7,7 @@ t_sprite *scr_player_get_spr(int type, int inverted, int dir)
 	void		*asset;
 	t_sprite	*spr;
 
-	idx = 2 + (inverted == S_STRAIGHT) * 10 + type * 2 + (dir != 1);
+	idx = 4 + (inverted == S_STRAIGHT) * 10 + type * 2 + (dir != 1);
 	asset = &(g()->asset);
 	spr = *(t_sprite **)(asset + sizeof(void *) * idx);
 	return (spr);
