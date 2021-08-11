fa0e50a38f4180ad2d4bdd8def3e47aaf82f75da
차영훈 <chayeonghun@chayeonghun-ui-MacBookPro.local>
2021-08-11 19:00:15 +0900

    so long makefile fixed

---
+++ so_long/src/asset/script/scr_find_ins_condition.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   scr_alive_zombie.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: su <su@student.42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/10 16:45:41 by su                #+#    #+#             */
+/*   Updated: 2021/08/10 20:26:28 by su               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "engine.h"
+
+t_instance	*scr_find_dead_zombie(void)
+{
+	t_instance	*dead_zombie;
+	t_instance	*ins;
+	t_list		*node;
+
+	dead_zombie = 0;
+	node = g()->instances[ZOMBIE_BLUE]->next;
+	while (node)
+	{
+		ins = node->data;
+		node = node->next;
+		if (ins->condition & C_DEAD)
+		{
+			ins->condition = C_ALIVE;
+			dead_zombie = ins;
+		}
+	}
+	return (dead_zombie);
+}
+
+t_instance	*scr_find_alive_gold(void)
+{
+	t_instance	*alive_gold;
+	t_instance	*ins;
+	t_list		*node;
+
+	alive_gold = 0;
+	node = g()->instances[ZOMBIE_BLUE]->next;
+	while (node)
+	{
+		ins = node->data;
+		node = node->next;
+		if (ins->condition & C_ALIVE)
+			alive_gold = ins;
+	}
+	return (alive_gold);
+}
