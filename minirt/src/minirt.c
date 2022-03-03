df6a91eaacf55123b673356be0ac8ec8002f630b
huni <petercha2000@gmail.com>
2022-03-03 20:44:19 +0900

    Refactor: minirt 디렉토리 구조 변경, makefile 수정
    Co-authored-by: Euimin Chung <euiminnn@users.noreply.github.com>

--- minirt/src/minirt.c
+++ minirt/src/minirt.c
@@ -12,19 +12,19 @@
 
 #include "minirt.h"
 
-static void	event(t_minirt *mini)
+static void event(t_minirt *mini)
 {
 	mlx_key_hook(mini->scr.win, key_hook, mini);  // esc close
 	mlx_hook(mini->scr.win, 17, 0, exit_hook, 0); // x button close
 }
 
-static void	setting(t_minirt *mini)
+static void setting(t_minirt *mini)
 {
-	t_camera	*c;
+	t_camera *c;
 
 	mini->scr.mlx = mlx_init();
 	mini->scr.win = mlx_new_window(
-		mini->scr.mlx, mini->scr.wid, mini->scr.hei, "이게 미니 알티다");
+		mini->scr.mlx, mini->scr.wid, mini->scr.hei, "this is minirt");
 	mini->scr.anti = 0;
 	c = mini->cam;
 	while (c->next)
@@ -34,7 +34,7 @@ static void	setting(t_minirt *mini)
 	event(mini);
 }
 
-int			init(t_minirt *mini)
+int init(t_minirt *mini)
 {
 	ft_memset(mini, 0, sizeof(t_minirt));
 	mini->wrd = init_world();
@@ -45,9 +45,9 @@ int			init(t_minirt *mini)
 	return (OK);
 }
 
-int			main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	t_minirt	mini;
+	t_minirt mini;
 
 	if (!init(&mini) || !input(argc, argv, &mini))
 		return (ERROR);
