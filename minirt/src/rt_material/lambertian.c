bde449e49a0750f432452b4a2dc41f06b25b41de
Euimin Chung <jane3079@korea.ac.kr>
2022-03-07 00:32:31 +0900

    Refactor: minirt material 폴더 norminette
    Co-authored-by: huni <Skyrich2000@users.noreply.github.com>

--- minirt/src/rt_material/lambertian.c
+++ minirt/src/rt_material/lambertian.c
@@ -1,20 +1,26 @@
 #include "minirt.h"
 
-t_material create_lambertian(t_vec color)
+t_material	create_lambertian(t_vec color)
 {
-	t_material mat;
+	t_material	mat;
 
 	mat.color = vec_divn(color, 255);
 	mat.scatter = scatter_lambertian;
 	return (mat);
 }
 
-int scatter_lambertian(t_material *this, t_ray *ray_in, t_hit_record *rec, t_ray *ray_out)
-{
-	t_vec target;
+// or random_unit_vector + near_zero
+// random value에 의한 정점좌표를 새로 지정
+int	scatter_lambertian(
+	t_material *this,
+	t_ray *ray_in,
+	t_hit_record *rec,
+	t_ray *ray_out
+) {
+	t_vec	target;
 
-	target = random_in_hemisphere(&rec->n); // or random_unit_vector + near_zero
-	ray_out->origin = rec->p; // random value에 의한 정점좌표를 새로 지정
+	target = random_in_hemisphere(&rec->n);
+	ray_out->origin = rec->p;
 	ray_out->dir = target;
 	return (1);
 }
