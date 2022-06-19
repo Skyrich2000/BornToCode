3e73b51f681d2fcfdf6f52d4119db0c288ed5922
ycha <petercha2000@gmail.com>
2022-06-19 23:00:40 +0900

    Fix: cpp module 06 01 add iter const

--- cpp_module/cpp_module_07/ex01/include/iter.hpp
+++ cpp_module/cpp_module_07/ex01/include/iter.hpp
@@ -4,7 +4,14 @@
 #include <iostream>
 
 template <typename T>
-void iter(T *array, size_t size, void (*f)(T const &)) {
+void iter(T const * array, size_t size, void (*f)(T const &)) {
+    for (size_t i = 0; i < size; i++) {
+        f(array[i]);
+    }
+}
+
+template <typename T>
+void iter(T *array, size_t size, void (*f)(T &)) {
     for (size_t i = 0; i < size; i++) {
         f(array[i]);
     }
